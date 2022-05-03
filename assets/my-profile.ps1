30f18f9b56bae215ca4f9d5d8fb117f9be2db386
guhcalm <54589986+guhcalm@users.noreply.github.com>
2022-05-03 03:19:12 -0400

    Add files via upload

--- assets/my-profile.ps1
+++ assets/my-profile.ps1
@@ -1,12 +1,5 @@
-# Prompt
-Import-Module posh-git
-Import-Module oh-my-posh
-
 # Load Theme #
-oh-my-posh --init --config $home/.config/powershell/minimalist-theme.omp.json | Invoke-Expression
-
-#Icons
-Import-Module Terminal-Icons
+oh-my-posh --init --config ~/.config/powershell/minimalist-theme.omp.json | Invoke-Expression
 
 # Alias
 Set-Alias touch ni
